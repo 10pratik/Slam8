c6372235588be7f3ff0371903992b97b1f4d4e81
subhambiswajit <digu35@gmail.com>
2016-11-01 14:55:47 +0530

    static root created

--- slamit/slamit/settings.py
+++ slamit/slamit/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'slamit_db',
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'digu13533',
+        'PASSWORD': '',
         # Empty for localhost through domain sockets or '127.0.0.1' for localhost
         # through TCP.
         'HOST': 'localhost',
@@ -128,3 +128,4 @@ TEMPLATE_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 ADMIN_MEDIA_PREFIX = '/media/'
+STATIC_ROOT = (os.path.join(BASE_DIR, 'staticstorage'))
